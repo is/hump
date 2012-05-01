bcd5b9d1a2518861a44d9fca47901dc890fde1b7
Xin Yu <xin.yu@renren-inc.com>
2012-05-01 10:53:47 +0800

    Add more codes from SleepJob.java
    - Set output format to NullOutputFormat.

--- src/main/java/us/yuxin/hump/HumpJob.java
+++ src/main/java/us/yuxin/hump/HumpJob.java
@@ -4,18 +4,30 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 
 public class HumpJob {
   public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
     Configuration conf = new Configuration();
+
+    conf.setBoolean("mapred.map.tasks.speculative.execution", false);
+    conf.setBoolean("mapred.reduce.tasks.speculative.execution", false);
     Job job = new Job(conf);
 
     job.setJobName("Hump-Sample");
 
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(NullWritable.class);
+    job.setOutputFormatClass(NullOutputFormat.class);
+
     job.setInputFormatClass(HumpInputFormat.class);
+    FileInputFormat.addInputPath(job, new Path("ignored"));
+
     job.setJarByClass(HumpMapper.class);
     job.waitForCompletion(true);
   }
