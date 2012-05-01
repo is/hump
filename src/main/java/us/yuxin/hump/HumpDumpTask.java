e49108f976cde313d002d25aae8c698bd01d1272
Xin Yu <xin.yu@renren-inc.com>
2012-05-01 23:02:56 +0800

    Add exceptions.

--- src/main/java/us/yuxin/hump/HumpDumpTask.java
+++ src/main/java/us/yuxin/hump/HumpDumpTask.java
@@ -1,21 +1,22 @@
 package us.yuxin.hump;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class HumpDumpTask implements HumpTask {
   @Override
-  public void setup(Mapper.Context context) {
-    //To change body of implemented methods use File | Settings | File Templates.
+  public void setup(Mapper.Context context)  throws IOException, InterruptedException {
   }
 
   @Override
-  public void run(Mapper.Context context, Text serial, Text taskInfo) {
+  public void run(Mapper.Context context, Text serial, Text taskInfo) throws IOException, InterruptedException {
     System.out.println("HumpDumpTask.run -- " + serial.toString() + ":" + taskInfo.toString());
+    Thread.sleep(1500);
   }
 
   @Override
-  public void cleanup(Mapper.Context context) {
-    //To change body of implemented methods use File | Settings | File Templates.
+  public void cleanup(Mapper.Context context) throws IOException, InterruptedException  {
   }
 }
--- src/main/java/us/yuxin/hump/HumpTask.java
+++ src/main/java/us/yuxin/hump/HumpTask.java
@@ -1,10 +1,12 @@
 package us.yuxin.hump;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public interface HumpTask {
-  public void setup(Mapper.Context context);
-  public void run(Mapper.Context context, Text serial, Text taskInfo);
-  public void cleanup(Mapper.Context context);
+  public void setup(Mapper.Context context) throws IOException, InterruptedException ;
+  public void run(Mapper.Context context, Text serial, Text taskInfo) throws IOException, InterruptedException;
+  public void cleanup(Mapper.Context context) throws IOException, InterruptedException;
 }
