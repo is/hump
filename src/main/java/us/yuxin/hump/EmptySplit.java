8c866107b0b823cd7cd12b2859890e0e5efb4db8
Xin Yu <xin.yu@renren-inc.com>
2012-05-01 11:08:52 +0800

    EmptySplit implements Writable.

--- src/main/java/us/yuxin/hump/EmptySplit.java
+++ src/main/java/us/yuxin/hump/EmptySplit.java
@@ -4,9 +4,10 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputSplit;
 
-public class EmptySplit extends InputSplit {
+public class EmptySplit extends InputSplit implements Writable {
   public void write(DataOutput out) throws IOException { }
   public void readFields(DataInput in) throws IOException { }
   public long getLength() { return 0L; }
