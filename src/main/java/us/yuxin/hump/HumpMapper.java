103aaeb8146b6cf4c9c25bd7bdb0d2079468b3f7
Xin Yu <xin.yu@renren-inc.com>
2012-05-01 14:23:17 +0800

    Change Log to org.apache.hadoop.mapred.Task

--- src/main/java/us/yuxin/hump/HumpMapper.java
+++ src/main/java/us/yuxin/hump/HumpMapper.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class HumpMapper extends Mapper<Text, Text, Text, NullWritable> {
-  private static Log log = LogFactory.getLog(HumpMapper.class);
+  private static Log log = LogFactory.getLog("org.apache.hadoop.mapred.Task");
 
   @Override
   protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
