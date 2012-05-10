333e9d2285b263411c862d65f0ce64e7290c4ee2
Xin Yu <xin.yu@renren-inc.com>
2012-05-10 17:59:36 +0800

    Create hive table schema from database source. #HUMP-7

--- src/main/java/us/yuxin/hump/cli/Gen.java
+++ src/main/java/us/yuxin/hump/cli/Gen.java
@@ -1,6 +1,7 @@
 package us.yuxin.hump.cli;
 
 import java.io.PrintStream;
+import java.sql.SQLException;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
@@ -10,6 +11,8 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import us.yuxin.hump.JdbcSource;
+import us.yuxin.hump.JdbcSourceMetadata;
 
 public class Gen {
   CommandLine cmdline;
@@ -30,11 +33,39 @@ public class Gen {
   final static String O_COLUMNS = "columns";
   final static String O_COMMENT = "comment";
   final static String O_DRIVER = "driver";
+  final static String O_EXTERNAL = "external";
+  final static String O_FLAGS = "flags";
   final static String O_HIVE_TYPES = "hivetypes";
+  final static String O_QUERY = "query";
   final static String O_SOURCE = "source";
   final static String O_SQL_TYPES = "sqltypes";
   final static String O_TABLE = "table";
   final static String O_TARGET = "target";
+  final static String O_USERNAME = "username";
+  final static String O_PASSWORD = "password";
+
+
+  private void prepareCmdlineOptions() {
+    options = new Options();
+    addOption("d", O_DRIVER, true, "JDBC Driver classname", "class");
+    addOption("s", O_SOURCE, true, "Data source URL", "url");
+    addOption("Q", O_QUERY, true, "SQL query statement", "statement");
+    addOption(null, O_TABLE, true, "Data source table name", "name");
+
+    addOption("C", O_COLUMNS, true, "Column names", "names");
+    addOption("H", O_HIVE_TYPES, true, "Column hive types", "types");
+    addOption("S", O_SQL_TYPES, true, "Column SQL types", "types");
+
+    addOption("t", O_TARGET, true, "Target table name", "name");
+    addOption(null, O_COMMENT, true, "Table comments", "comment");
+
+    addOption("U", O_USERNAME, true, "Database username", "user");
+    addOption("P", O_PASSWORD, true, "Database password", "pass");
+
+    addOption("f", O_FLAGS, true, "Flags for hive DML statement\ne:EXTERNAL, i:IF NOT EXIST, d:DROP TABLE", "flags");
+    addOption("e", O_EXTERNAL, false, "External hive table");
+  }
+
 
   public static void main(String argv[]) throws Exception {
     Gen app = new Gen();
@@ -46,19 +77,57 @@ public class Gen {
     prepareCmdlineOptions();
 
     if (args.length == 0) {
-      HelpFormatter formmatter = new HelpFormatter();
-      formmatter.printHelp("gen", options);
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("gen", options);
       System.exit(0);
     }
 
     parseCmdline(OptionsFileUtil.expandArguments(args));
 
     if (cmdline.hasOption(O_COLUMNS) && cmdline.hasOption(O_HIVE_TYPES)) {
-      genByHiveColumnDescripter();
+      genByHiveColumnDescriptor();
+    }
+
+    if (cmdline.hasOption(O_SOURCE)) {
+      genByDatasource();
     }
   }
 
-  private void genByHiveColumnDescripter() {
+  private void genByDatasource() throws ClassNotFoundException, SQLException {
+    String url = cmdline.getOptionValue(O_SOURCE);
+    String driver = cmdline.getOptionValue(O_DRIVER);
+    String table = cmdline.getOptionValue(O_TABLE);
+    String query = cmdline.getOptionValue(O_QUERY);
+
+    if (driver.equals("mysql")) {
+      driver = "com.mysql.jdbc.Driver";
+    }
+
+    if (query == null) {
+      query = "SELECT * FROM " + table + " LIMIT 1;";
+    }
+
+    JdbcSource source = new JdbcSource();
+    source.setDriver(driver);
+    source.setUrl(url);
+    source.setQuery(query);
+    source.setUsername(cmdline.getOptionValue(O_USERNAME));
+    source.setPassword(cmdline.getOptionValue(O_PASSWORD));
+    source.open();
+
+    JdbcSourceMetadata meta = new JdbcSourceMetadata();
+    meta.setJdbcSource(source);
+
+    columnNames = meta.names;
+    columnTypes = meta.getHiveTypeNames();
+    source.close();
+
+    buildTable();
+    System.exit(0);
+  }
+
+
+  private void genByHiveColumnDescriptor() {
     String columns = cmdline.getOptionValue(O_COLUMNS);
     String hivetypes = cmdline.getOptionValue(O_HIVE_TYPES);
 
@@ -110,54 +179,50 @@ public class Gen {
   }
 
 
-  private void prepareCmdlineOptions() {
-    options = new Options();
-    Option o;
-
-    o = new Option("d", O_DRIVER, true, "JDBC Driver classname");
-    o.setArgName("class");
-    options.addOption(o);
-
-    o = new Option("s", O_SOURCE, true, "Data source URL");
-    o.setArgName("url");
-    options.addOption(o);
-
-    o = new Option(null, O_TABLE, true, "Data source table name");
-    o.setArgName("name");
-    options.addOption(o);
-
-    o = new Option("C", O_COLUMNS, true, "Column names");
-    o.setArgName("names");
-    options.addOption(o);
-
-    o = new Option("H", O_HIVE_TYPES, true, "Column hive types");
-    o.setArgName("types");
-    options.addOption(o);
-
-    o = new Option("S", O_SQL_TYPES, true, "Column SQL types");
-    o.setArgName("types");
-    options.addOption(o);
-
-    o = new Option("t", O_TARGET, true, "Target table name");
-    o.setArgName("name");
-    options.addOption(o);
-
-    o = new Option(null, O_COMMENT, true, "Table comments");
-    o.setArgName("comment");
-    options.addOption(o);
-  }
-
-
   private void parseCmdline(String args[]) throws ParseException {
     GnuParser parser = new GnuParser();
     cmdline = parser.parse(options, args, null, false);
   }
 
+
+  private void addOption(String shortOpt, String longOpt, boolean hasArg, String description) {
+    Option o = new Option(shortOpt,longOpt, hasArg, description);
+    options.addOption(o);
+  }
+
+
+  private void addOption(String shortOpt, String longOpt, boolean hasArg, String description, String argName) {
+    Option o = new Option(shortOpt,longOpt, hasArg, description);
+    o.setArgName(argName);
+    options.addOption(o);
+  }
+
+
   private void buildTable() {
     prepareStream();
     prepareTablename();
 
-    out.println("CREATE TABLE " + tableName + " (");
+    String flags = cmdline.getOptionValue(O_FLAGS, "");
+
+
+    if (flags.indexOf('d') != -1) {
+      out.println("DROP TABLE IF EXISTS " + tableName + ";");
+    }
+
+    String external;
+    String ifNotExists;
+    if (flags.indexOf('e') != -1 || cmdline.hasOption(O_EXTERNAL))
+      external = "EXTERNAL ";
+    else
+      external = "";
+
+    if (flags.indexOf('i') != -1)
+      ifNotExists = "IF NOT EXISTS ";
+    else
+      ifNotExists = "";
+
+
+    out.println("CREATE " + external + "TABLE " + ifNotExists + tableName + " (");
     for (int i = 0; i < columnNames.length; ++i) {
       out.print(columnNames[i] + " " + columnTypes[i]);
       if (i != (columnNames.length - 1))
