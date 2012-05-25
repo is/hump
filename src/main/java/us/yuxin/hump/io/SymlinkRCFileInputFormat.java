fb311cdedd7bf6b5c960681e637111c4d7f60bdf
Xin Yu <xin.yu@renren-inc.com>
2012-05-25 11:35:55 +0800

    Implment ContentSummaryInputFormat, ReworkMapredInputFormat. HUMP-40 #ss

--- src/main/java/us/yuxin/hump/io/SymlinkRCFileInputFormat.java
+++ src/main/java/us/yuxin/hump/io/SymlinkRCFileInputFormat.java
@@ -5,15 +5,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.ContentSummary;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.io.ContentSummaryInputFormat;
 import org.apache.hadoop.hive.ql.io.RCFileInputFormat;
 import org.apache.hadoop.hive.ql.io.RCFileRecordReader;
+import org.apache.hadoop.hive.ql.io.ReworkMapredInputFormat;
+import org.apache.hadoop.hive.ql.plan.MapredWork;
+import org.apache.hadoop.hive.ql.plan.PartitionDesc;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
@@ -25,7 +32,7 @@ import org.apache.hadoop.mapred.Reporter;
 
 
 public class SymlinkRCFileInputFormat<K extends LongWritable, V extends BytesRefArrayWritable>
-  extends RCFileInputFormat<K, V> {
+  extends RCFileInputFormat<K, V> implements ContentSummaryInputFormat, ReworkMapredInputFormat {
 
   long minSplitSize = SequenceFile.SYNC_INTERVAL;
   long blockSize = 1024 * 1024 * 64;
@@ -45,9 +52,8 @@ public class SymlinkRCFileInputFormat<K extends LongWritable, V extends BytesRef
 		Reporter reporter) throws IOException {
 
 		reporter.setStatus(split.toString());
-		FileSplit s = (FileSplit)split;
-		LOG.info("GETRR: " + s.getPath().toString() + ":" + s.getLength());
-
+		// FileSplit s = (FileSplit)split;
+		// LOG.info("GETRR: " + s.getPath().toString() + ":" + s.getLength());
 		return new RCFileRecordReader(job, (FileSplit) split);
 	}
 
@@ -61,7 +67,6 @@ public class SymlinkRCFileInputFormat<K extends LongWritable, V extends BytesRef
   @Override
   public InputSplit[] getSplits(JobConf job, int numSplits) throws IOException {
     Path[] symlinksDirs = getInputPaths(job);
-		LOG.info("GET-SPLITS");
 
     if (symlinksDirs.length == 0) {
       throw new IOException("No input paths specified in job");
@@ -143,6 +148,80 @@ public class SymlinkRCFileInputFormat<K extends LongWritable, V extends BytesRef
     }
 
   }
+
+  @Override
+  public ContentSummary getContentSummary(Path p, JobConf job) throws IOException {
+    long[] summary = {0, 0, 0};
+    List<FileInfo> targetPaths = new ArrayList<FileInfo>();
+    try {
+      getTargetPathsFromSymlinksDirs(job, new Path[] {p}, targetPaths);
+    } catch (Exception e) {
+      throw new IOException("Error parsing symlinks from specified job input path.", e);
+    }
+
+    for (FileInfo fi: targetPaths) {
+      summary[0] += fi.length;
+      summary[1] += 1;
+    }
+
+    return new ContentSummary(summary[0], summary[1], summary[2]);
+  }
+
+  @Override
+  public void rework(HiveConf job, MapredWork work) throws IOException {
+    Map<String, PartitionDesc> pathToParts = work.getPathToPartitionInfo();
+    List<String> toRemovePaths = new ArrayList<String>();
+    Map<String, PartitionDesc> toAddPathToPart = new HashMap<String, PartitionDesc>();
+    Map<String, ArrayList<String>> pathToAliases = work.getPathToAliases();
+
+    for (Map.Entry<String, PartitionDesc> pathPartEntry: pathToParts.entrySet()) {
+      String path = pathPartEntry.getKey();
+      PartitionDesc partDesc = pathPartEntry.getValue();
+
+      if (partDesc.getInputFileFormatClass().equals(SymlinkRCFileInputFormat.class)) {
+        Path symlinkDir = new Path(path);
+        FileSystem fileSystem = symlinkDir.getFileSystem(job);
+        FileStatus fStatus = fileSystem.getFileStatus(symlinkDir);
+
+        FileStatus symlinks[] = null;
+        if (!fStatus.isDir()) {
+          symlinks = new FileStatus[] {fStatus};
+        } else {
+          symlinks = fileSystem.listStatus(symlinkDir);
+        }
+
+        toRemovePaths.add(path);
+        ArrayList<String> aliases = pathToAliases.remove(path);
+        for (FileStatus symlink: symlinks) {
+          BufferedReader reader = new BufferedReader(
+            new InputStreamReader(fileSystem.open(symlink.getPath())));
+          String line;
+          line = reader.readLine();
+          if (line.equals(SYMLINK_FILE_SIGN_V1)) {
+            while((line = reader.readLine()) != null) {
+              int o1 = line.indexOf(',');
+              int o2 = line.indexOf(',', o1 + 1);
+              long row = Long.parseLong(line.substring(o1 + 1, o2));
+              String realPath = line.substring(o2 + 1);
+              if (row == 0) {
+                continue;
+              }
+
+              toAddPathToPart.put(realPath, partDesc);
+              pathToAliases.put(line, aliases);
+            }
+          }
+          reader.close();
+        }
+      }
+    }
+
+    pathToParts.putAll(toAddPathToPart);
+    for (String toRemove: toRemovePaths) {
+      pathToParts.remove(toRemove);
+    }
+  }
+
 
   public static class FileInfo {
     public Path path;
