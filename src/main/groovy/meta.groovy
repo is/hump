0dca2b1713da3da2978d51ab65d4b958964d4943
Xin Yu <xin.yu@renren-inc.com>
2012-06-28 11:44:01 +0800

    Add Grape dependencies on HADOOP. HUMP-64

--- src/main/groovy/meta.groovy
+++ src/main/groovy/meta.groovy
@@ -1,6 +1,9 @@
-import org.apache.hadoop.conf.Configuration
+@Grab(group ="org.apache.hadoop", module="hadoop-common", version="2.0.0-alpha")
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 
 Configuration conf = new Configuration();
-FileSystem fs = FileSystem.getFileSystem(conf);
+FileSystem fs = FileSystem.get(conf)
 
 // vim: ts=2 sts=2 ai
