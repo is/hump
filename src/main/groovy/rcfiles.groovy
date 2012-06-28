436ddc558932db759eeaf77d9a37328d206c61ac
Xin Yu <xin.yu@renren-inc.com>
2012-06-28 21:45:45 +0800

    Create table tools for RCFile.

---
+++ src/main/groovy/rcfiles.groovy
@@ -0,0 +1,24 @@
+import groovy.json.JsonSlurper
+
+String tablename = args[0]
+String location = args[1]
+String descriptionFile = args[2]
+
+def descriptor = new JsonSlurper().parse(new FileReader(descriptionFile));
+
+String columns = descriptor.columns as String;
+String columnTypes = descriptor.columnTypes as String;
+
+String[] columnArray = columns.split(",");
+String[] columnTypeArray = columnTypes.split(":");
+
+println """CREATE EXTERNAL TABLE ${tablename} ("""
+print("${columnArray[0]} ${columnTypeArray[0]}")
+for (int i = 1; i < columnArray.length; ++i) {
+	println(",")
+	print("${columnArray[i]} ${columnTypeArray[i]}")
+}
+
+println(")")
+println("STORED AS RCFILE")
+println("LOCATION '${location}';")
